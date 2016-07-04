718c233e7023a4a9557ad6145c031eb65a207362
Federico Farias <federico.farias@optimissa.com.mx>
2016-07-04 13:19:29 -0500

    Avance del módulo editar póliza

--- app/scripts/controllers/polizas/polizaseditar.js
+++ app/scripts/controllers/polizas/polizaseditar.js
@@ -8,10 +8,26 @@
  * Controller of the sistemaPolizasPgApp
  */
 angular.module('sistemaPolizasPgApp')
-  .controller('PolizasEditarCtrl', function () {
+  .controller('PolizasEditarCtrl', function (
+  	$scope,
+  	$stateParams,
+  	PolizasService) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.poliza;
+
+    var idPoliza = $stateParams.idPoliza;
+
+    PolizasService.find(idPoliza, function(data) {
+    	$scope.poliza = data;
+    	/*
+        $scope.afianzado = data.Afianzado;
+        $scope.AveriguacionPrevia = data.AveriguacionPrevia;
+        */
+    });
+
   });
