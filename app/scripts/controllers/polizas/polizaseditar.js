7bc412c6dd14672d189d54756cd2aa2f8fc3621c
Federico Farias <federico.farias@optimissa.com>
2016-09-16 14:38:02 -0500

    Avance de edición de pólizas

--- app/scripts/controllers/polizas/polizaseditar.js
+++ app/scripts/controllers/polizas/polizaseditar.js
@@ -11,23 +11,30 @@ angular.module('sistemaPolizasPgApp')
   .controller('PolizasEditarCtrl', function (
   	$scope,
   	$stateParams,
-  	PolizasService) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  	PolizasService,
+    AfianzadoraService) {
 
+    $scope.nomAfianzadoraActual;
+    $scope.nomCompletoAfianzado;
     $scope.poliza;
 
     var idPoliza = $stateParams.idPoliza;
 
     PolizasService.find(idPoliza, function(data) {
     	$scope.poliza = data;
+      $scope.nomCompletoAfianzado = $scope.poliza.Afianzado.ApellidoPaterno + ' ' + $scope.poliza.Afianzado.ApellidoMaterno + ' ' + $scope.poliza.Afianzado.Nombre;
+      // $scope.nomAfianzadoraActual = angular.copy($scope.poliza.Afianzadora.Nombre);
+      $scope.nomAfianzadoraActual = $scope.poliza.Afianzadora.Nombre;
     	/*
         $scope.afianzado = data.Afianzado;
         $scope.AveriguacionPrevia = data.AveriguacionPrevia;
         */
+    });
+
+    $scope.loaderAfianzadora = true;
+    $scope.afianzadoras = AfianzadoraService.all(function(data) {
+      $scope.afianzadoras = data;
+      $scope.loaderAfianzadora = false;
     });
 
   });
